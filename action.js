66027831fff917b8cc49acd04f524fbffb80a73f
leddins <leddins@gmail.com>
2020-05-04 00:19:48 -0400

    Everything is in working order. The logic needs to be cleaned up though and the functionality of the operator buttons should be finessed.

--- action.js
+++ action.js
@@ -1,18 +1,39 @@
-const numbers = document.querySelectorAll(".numbers");
-numbers.forEach(number => number.addEventListener("click", numberInput))
-
 const display = document.querySelector("#display");
 let leftNum = "";
 let rightNum = "";
-let currentOper;
+let currentOper = "enter";
+delayClear = false;
+
+const numbers = document.querySelectorAll(".numbers");
+numbers.forEach(number => number.addEventListener("click", numberInput))
 
 const operators = document.querySelectorAll(".oper");
 operators.forEach(operator => operator.addEventListener("click", process));
 
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", clearField);
+
+const deleteOne = document.querySelector("#delete");
+deleteOne.addEventListener("click", deleteDig);
+
+
+
+function clearField(){
+  display.textContent = "";
+  rightNum = "";
+  leftNum = "";
+  currentOper = "enter";
+}
+
+function deleteDig(){
+  console.log("delayClear = " + delayClear)
+  if (delayClear) return;
+  display.textContent = display.textContent.slice(0, display.textContent.length - 1);
+}
 
 function operate(oper, a, b){
   if (oper == "plus"){
-    return +a + +b;
+    return a + b;
   } else if (oper == "minus"){
     return a - b;
   } else if (oper == "times"){
@@ -20,11 +41,15 @@ function operate(oper, a, b){
   } else if (oper == "by"){
     return a / b;
   } else {
-    return null;
+    return display.textContent;
   }
 }
 
 function numberInput(e){
+  if (delayClear){
+    display.textContent = "";
+    delayClear = false;
+  }
   if (display.textContent.length >= 14) return;
   const digit = e.target.textContent;
   if (digit == "." && display.textContent.includes(".")) return;
@@ -32,25 +57,15 @@ function numberInput(e){
 }
 
 function process(e){
+  if (delayClear) return;
   rightNum = display.textContent;
-  display.textContent = "";
-  if (e.target.id == "delete"){
-    display.textContent = rightNum.slice(0, rightNum.length - 1);
-    rightNum = display.textContent;
-    return;
-  } else if (e.target.id == "clear"){
-    rightNum = "";
-    leftNum = "";
-    return;
+  let result = +operate(currentOper, +leftNum, +rightNum);
+  if (result.toString().length >= 14){
+    result = result.toPrecision(11);
   }
-  if (leftNum){
-    let result = operate(currentOper, leftNum, rightNum);
-    if (result.toString().length >= 14){
-      result = result.toPrecision(12);
-    }
-    display.textContent = result;
-  }
+  display.textContent = result.toString();
+  console.log(leftNum + " " + currentOper + " " + rightNum + " = " + result);
+  leftNum = result;
+  delayClear = true;
   currentOper = e.target.id
-  leftNum = rightNum;
-  rightNum = "";
 }
